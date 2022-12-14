e571e53e5b8aeb3b9280fdb188407c0a25903f98
HenriqueMVSS <henrique_mateus1994@hotmail.com>
2022-12-14 04:29:26 -0300

    apollo provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Layout } from '../src/layout/Layout'
+import { ApolloProvider, apolloClient } from '../src/apolloClient'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      
-      <Component {...pageProps} />
-    
-    </Layout>
-    
+      <ApolloProvider client={apolloClient}>
+        <Layout>
+          
+          <Component {...pageProps} />
+        
+        </Layout>
+      </ApolloProvider>
     )
   }
   
